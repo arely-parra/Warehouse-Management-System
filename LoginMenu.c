18cfa72b36bd73985d57f03ed0bb4bb388ab40f4
arely-parra <80774483+arely-parra@users.noreply.github.com>
2021-04-24 18:08:11 -0400

    Add files via upload

--- LoginMenu.c
+++ LoginMenu.c
@@ -1,6 +1,6 @@
 //* Project 4: Warehouse Management System
 //* Group Name: Can You C My Screen?
-//* Group Members: Arely J. Parra López, Jarred Crystal, Alexander Vita, Kamyrn Hammond, and Sophie Ross
+//* Group Members: Arely J. Parra López, Jarred Crystal, Alexander Vita, Kamyrn Hammond, & Sophie Ross
 //* Course: CSCI 1110-01 | Introduction to C Programming
 //* Professor: Dr. Reza Sadeghi
 //* Date: Monday, April 26th, 2021
