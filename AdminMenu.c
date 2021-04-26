fdf2a86c9bbff22e964c3791ba0bf3fc51e20ca3
arely-parra <80774483+arely-parra@users.noreply.github.com>
2021-04-25 21:45:37 -0400

    Update and rename AdminMenu_Vita Rev to AdminMenu.c

--- AdminMenu.c
+++ AdminMenu.c
@@ -1,4 +1,9 @@
-
+//* Project 4: Warehouse Management System
+//* Group Name: Can You C My Screen?
+//* Group Members: Arely J. Parra López, Jarred Crystal, Alexander Vita, Kamyrn Hammond, and Sophie Ross
+//* Course: CSCI 1110-01 | Introduction to C Programming
+//* Professor: Dr. Reza Sadeghi
+//* Date: Monday, April 26th, 2021
 
 #include <stdio.h>
 #include <stdlib.h>
